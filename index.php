7720360ddab8dbc76ea14493c0a5fc018fb188c0
olivier poussel <olivier.poussel@gmail.com>
2022-01-26 11:36:16 +0100

    save 26/01/22 midi

--- index.php
+++ index.php
@@ -1,40 +1,25 @@
 <?php
+require_once('./vendor/autoload.php');
 
+use App\Entity\Acteur;
+use App\Repository\ActeurRepository;
 
-//connexion a PDO
-$username= 'root';
-$pwd = '';
-$dsn = 'mysql://host=localhost;port=3306;dbname=cinema';
-$connexion = new PDO($dsn, $username, $pwd, []);
+$acteur = new ActeurRepository();
 
-//en mode console ou html
-// var_dump($resltat);
-$nom = 'poussel';
-$prenom = 'olivier';
-$sqlInsert = "INSERT INTO acteur(nom, prenom) VALUES(:nom,:prenom)";
-$statement = $connexion->prepare($sqlInsert);
-$statement->execute(['nom'=> $nom, 'prenom'=> $prenom]);
-$id = $connexion->lastInsertId();
+$resultat = $acteur->findAll();
 
-$sqlUpdate = 'UPDATE acteur SET nom = :nom, prenom = :prenom WHERE id = :id';
-$statement = $connexion->prepare($sqlUpdate);
-$statement->execute(['id'=> $id, 'nom'=> 'Doe', 'prenom'=> 'john']);
-
-//requete et affichage des Acteurs
-$sql = 'SELECT * FROM acteur';
-$statement = $connexion->query($sql);
-$statement->execute();
-$resltat = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BDD CESI</title>
 </head>
+
 <body>
     <h1>Acteur</h1>
 
@@ -47,7 +32,7 @@ $resltat = $statement->fetchAll(PDO::FETCH_ASSOC);
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($resltat as $acteur) :  ?>
+            <?php foreach ($resultat as $acteur) :  ?>
                 <tr>
                     <td><?= $acteur['id']; ?></td>
                     <td><?= $acteur['nom']; ?></td>
@@ -57,8 +42,5 @@ $resltat = $statement->fetchAll(PDO::FETCH_ASSOC);
         </tbody>
     </table>
 </body>
+
 </html>
-
-
-
-
---
+++ src/repository/acteurRepository.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Acteur;
+use PDO;
+
+class ActeurRepository
+{
+    //connexion a PDO
+    private $username = 'root';
+    private $pwd = '';
+    private $dsn = 'mysql://host=localhost;port=3306;dbname=cinema';
+
+    /** @var PDO */
+    private $connexion;
+
+    public function __construct()
+    {
+        $this->connexion = new PDO($this->dsn, $this->username, $this->pwd, []);
+    }
+
+    /**
+     * créer un acteur
+     *
+     * @param string $nom
+     * @param string $prenom
+     * @return integer
+     */
+    public function createActeur(string $nom, string $prenom): int
+    {
+        $sqlInsert = "INSERT INTO acteur(nom, prenom) VALUES(:nom,:prenom)";
+        $statement = $this->connexion->prepare($sqlInsert);
+        $statement->execute(['nom' => $nom, 'prenom' => $prenom]);
+        $id = (int) $this->connexion->lastInsertId();
+
+        return $id;
+    }
+
+    /**
+     * modifier un acteur
+     *
+     * @param integer $id
+     * @param string $nom
+     * @param string $prenom
+     * @return void
+     */
+    public function updateActeur(int $id, string $nom, string $prenom)
+    {
+        $sqlUpdate = 'UPDATE acteur SET nom = :nom, prenom = :prenom WHERE id = :id';
+        $statement = $this->connexion->prepare($sqlUpdate);
+        $statement->execute(['id' => $id, 'nom' => $nom, 'prenom' => $prenom]);
+    }
+
+
+    /**
+     * supprimer un acteur
+     *
+     * @param integer $id
+     * @return void
+     */
+    public function deleteActeur(int $id)
+    {
+        $sqlDelete = 'DELETE FROM acteur where id = :id';
+        $statement = $this->connexion->prepare($sqlDelete);
+        $statement->execute(['id' => $id]);
+    }
+
+    /**
+     * récupére tous les acteurs
+     *
+     * @return Acteur[] tableau d'acteur
+     */
+    public function findAll(): array
+    {
+        //requete et affichage des Acteurs
+        $sql = 'SELECT * FROM acteur';
+        $statement = $this->connexion->query($sql);
+        $statement->execute();
+        $resultat = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $acteurTab = [];
+        foreach ($resultat as $acteurArray) {
+            $acteur = new Acteur($acteurArray['id'], $acteurArray['nom'], $acteurArray['prenom']);
+            $acteurTab[] = $acteur;
+        }
+
+        return $acteurTab;
+    }
+
+    /**
+     * trouver un acteur particulier
+     *
+     * @param integer $id
+     * @return array
+     */
+    public function findById(int $id): array
+    {
+        $sql = 'SELECT * FROM acteur where id = :id';
+        $statement = $this->connexion->prepare($sql);
+        $statement->execute(['id' => $id]);
+        $resultat = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($resultat) {
+            $acteur = new Acteur($resultat['id'], $resultat['nom'], $resultat['prenom']);
+
+            return $acteur;
+        }
+
+        return $resultat;
+    }
+}
